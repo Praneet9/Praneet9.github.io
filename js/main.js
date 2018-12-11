916b11558dddb3c652bdf9dad4fa3e132d6c39c7
Praneet9 <praneetbomma@gmail.com>
2018-12-11 20:46:42 +0530

    SEO almost done

--- js/main.js
+++ js/main.js
@@ -5,20 +5,21 @@ var overlay = document.getElementById("loader");
 
                 overlay.style.display = 'none';
                 document.getElementById("body").style.display = 'inline';
-                let time = 0;
-                ScrollReveal().reveal('.reveal', { distance: '100px', origin: 'bottom', delay: 200, interval: 50 });
-                ScrollReveal().reveal('#hi_img', { distance: '100px', origin: 'bottom', delay: time += 100 });
-                ScrollReveal().reveal('#name', { distance: '100px', origin: 'bottom', delay: time += 100 });
-                ScrollReveal().reveal('#developer', { distance: '100px', origin: 'bottom', delay: 800 });
-                ScrollReveal().reveal('#myDescription', { distance: '100px', origin: 'bottom', delay: time += 100 });
-                ScrollReveal().reveal('#dwresume', { distance: '100px', origin: 'bottom', delay: time += 100 });
-                ScrollReveal().reveal('.navtitle', { distance: '100px', origin: 'left', delay: 100 });
-                ScrollReveal().reveal('.navlinks', { distance: '100px', origin: 'right', delay: 100 });
-                ScrollReveal().reveal('.skills', { distance: '100px', origin: 'bottom', delay: 200, interval: 50 });
-                ScrollReveal().reveal('#about_card', { distance: '100px', origin: 'left', delay: 400 });
-                ScrollReveal().reveal('#interested', { distance: '100px', origin: 'top', delay: 400 });
-                ScrollReveal().reveal('#hire_header', { distance: '100px', origin: 'bottom', delay: 200 });
-                ScrollReveal().reveal('#hiremesvg', { distance: '100px', origin: 'bottom', delay: 200 });
-                ScrollReveal().reveal('#myemail', { distance: '100px', origin: 'bottom', delay: 200 });
+                var time = 0;
+                var animate = ScrollReveal() 
+                animate.reveal('.reveal', { distance: '100px', origin: 'bottom', delay: 200, interval: 50 });
+                animate.reveal('#hi_img', { distance: '100px', origin: 'bottom', delay: time += 100 });
+                animate.reveal('#name', { distance: '100px', origin: 'bottom', delay: time += 100 });
+                animate.reveal('#developer', { distance: '100px', origin: 'bottom', delay: 800 });
+                animate.reveal('#myDescription', { distance: '100px', origin: 'bottom', delay: time += 100 });
+                animate.reveal('#dwresume', { distance: '100px', origin: 'bottom', delay: time += 100 });
+                animate.reveal('.navtitle', { distance: '100px', origin: 'left', delay: 100 });
+                animate.reveal('.navlinks', { distance: '100px', origin: 'right', delay: 100 });
+                animate.reveal('.skills', { distance: '100px', origin: 'bottom', delay: 200, interval: 50 });
+                animate.reveal('#about_card', { distance: '100px', origin: 'left', delay: 400 });
+                animate.reveal('#interested', { distance: '100px', origin: 'top', delay: 400 });
+                animate.reveal('#hire_header', { distance: '100px', origin: 'bottom', delay: 200 });
+                animate.reveal('#hiremesvg', { distance: '100px', origin: 'bottom', delay: 200 });
+                animate.reveal('#myemail', { distance: '100px', origin: 'bottom', delay: 200 });
             }, 1000);
         })
