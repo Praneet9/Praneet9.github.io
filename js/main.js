c113672b08364d45cc643c560bb3d66958328d6e
Praneet9 <praneetbomma@gmail.com>
2019-07-14 17:12:59 +0530

    working on dark mode

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ animate.reveal('#myemail', { distance: '100px', origin: 'bottom', delay: 200 });
 
 $('.owl-carousel').owlCarousel({
     loop: true,
-    margin: 5,
+    margin: 10,
     nav: false,
     responsive: {
         0: {
